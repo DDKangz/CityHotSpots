56d69a2886230800ec1ef20a9ddf006209c00477
Daniel Kang <danieldonghoonkang@gmail.com>
2019-09-28 16:46:23 -0700

    initial client-side set up

---
+++ client/server/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Components {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hotSpots : []
+    }
+  }
+
+  componentDidMount() {
+    //code
+  }
+
+  render() {
+    return (
+     //your code here 
+    )
+  }
+}
